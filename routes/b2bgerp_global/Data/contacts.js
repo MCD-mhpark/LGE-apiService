4768793fba060326d684a992280b651babacd739
djshin <comiclife84@gmail.com>
2021-01-19 13:05:11 +0900

    B2B GERP 필드 매핑 (진행중)

--- routes/b2bgerp_global/Data/contacts.js
+++ routes/b2bgerp_global/Data/contacts.js
@@ -1,6 +1,8 @@
+var moment = require('moment');
 var express = require('express');
 var router = express.Router();
 var httpRequest = require('../../common/httpRequest');
+var utils = require('../../common/utils');
 
 /* Contacts */
 
@@ -72,7 +74,7 @@ function B2B_GERP_GLOBAL_ENTITY(){
 //Eloqua Data B2B GERP Global Mapping 데이터 생성
 function Convert_B2BGERP_GLOBAL_DATA(contacts_data)
 {
-  
+
   var result_data = [];
   
   for( var i = 0; i < contacts_data.elements.length; i++)
@@ -82,7 +84,107 @@ function Convert_B2BGERP_GLOBAL_DATA(contacts_data)
     console.log(contacts_data.elements[i]);
     console.log(contacts_data.elements[i].accountName);
 
+    result_item.INTERFACE_ID = "Eloqua" // this.INTERFACE_ID = "Eloqua",
+
+    //리드네임 {{Business Unit}}_{{Subsidiary}}_{{Platform}}_{{Activity}}_{{Date}}
+    //리드네임 {{100229}}_{{100196}}_{{100202}}_{{100206}}
+    //100202	Platform&Activity
+    //100310	Platform&Activity History
+    //100026 Date Created
+    //100027 Date Modified
+    var s_BusinessUnit;
+    var s_Subsidiary;
+    var s_Platform_Action;
+    var s_Date;
+    for(var fieldValue in contacts_data.elements[i].fieldValues){
+      var fieldValue_id = contacts_data.elements[i].fieldValues[fieldValue].id;
+      var fieldValue_value = contacts_data.elements[i].fieldValues[fieldValue].value;
+
+      //리드네임 {{Business Unit}}_{{Subsidiary}}_{{Platform}}_{{Activity}}_{{Date}}
+      //리드네임 {{100229}}_{{100196}}_{{100202}}_{{100206}}
+      //순서 매핑 작업필요
+      if (fieldValue_id == 100229 || fieldValue_id == 100196 || fieldValue_id == 100202 || fieldValue_id == 100206 )
+      {
+        if (fieldValue != undefined) {
+          result_item.LEAD_NAME += fieldValue_value + '_';
+        }
+        else {
+          result_item.LEAD_NAME += 'NODATA_';
+        }
+      }
+
+      // result_item.ATTRIBUTE_7 = "";      //지역 - 국가 eloqua filed 정보
+      // region 100069
+      if(fieldValue_id == 100069)
+      {
+        if( fieldValue_value != undefined)
+        {
+          result_item.ATTRIBUTE_7 += fieldValue_value;
+        }
+      }
+
+    // result_item.ATTRIBUTE_8 = "";      //넷중 하나 또는 4개의 필드 정보 합 ( 확인 필요 )
+    // result_item.ATTRIBUTE_10 = "";     //데이터 없음
+    // result_item.ATTRIBUTE_11 = "";     //사업부코드( 코드마스터 필요 ) 예) HE    LGE 앞자리 빼는지 확인 필요
+    // result_item.REGISTER_DATE = "";    //어떤 날짜 정보인지 확인 필요
+    // result_item.TRANSFER_DATE = "";    //어떤 날짜 정보인지 확인 필요
+    // result_item.TRANSFER_FLAG = "";		//TRANSFER_FLAG N , Y 값의 용도 확인 필요
+    // result_item.LAST_UPDATE_DATE = ""; //데이터 없음
+    // result_item.API_G_CODE = "";       //API 구분코드 추가요건 사항
+
+    }
+    
+    //SITE_NAME ( 현장명 매핑필드 확인 )
+    result_item.SITE_NAME = "";
+
+    //LEAD_SOURCE_TYPE ( LGE에서 매핑 코드 따주시기로함 DEFAULT 09)
+    result_item.LEAD_SOURCE_TYPE = "09"
+
+    //ENTRY_TYPE ( LGE에서 매핑 코드 따주시기로함 DEFAULT 09)
+    result_item.ENTRY_TYPE = "L"
+
+    //ACCOUNT ( 회사 )
     if( contacts_data.elements[i].accountName != undefined) result_item.ACCOUNT = contacts_data.elements[i].accountName;
+    
+    // result_item.CONTACT_POINT = "";    //연락처(현업 협의 정의)
+    if( contacts_data.elements[i].mobilePhone != undefined) result_item.CONTACT_POINT = contacts_data.elements[i].mobilePhone;
+
+    // result_item.CORPORATION = "";      //법인정보
+    //if( contacts_data.elements[i].mobilePhone != undefined) result_item.CONTACT_POINT = contacts_data.elements[i].mobilePhone;
+
+    // result_item.OWNER = "";            //데이터 없음
+
+    // result_item.ADDRESS = "";          //현업확인
+    if( contacts_data.elements[i].address1 != undefined) result_item.ADDRESS = contacts_data.elements[i].address1;
+
+    // result_item.DESCRIPTION = "";      //설명 Comments, message, inquiry-to-buy-message 필드 중 하나
+    if( contacts_data.elements[i].description != undefined) result_item.DESCRIPTION = contacts_data.elements[i].description;
+
+    // result_item.ATTRIBUTE_3 = "";      //픽리스트 eloqua 확인 
+
+    // result_item.ATTRIBUTE_4 = "";      //이메일
+    if( contacts_data.elements[i].emailAddress != undefined) result_item.ATTRIBUTE_4 = contacts_data.elements[i].emailAddress;
+
+    // result_item.ATTRIBUTE_5 = "";      //전화번호
+    if( contacts_data.elements[i].businessPhone != undefined) result_item.ATTRIBUTE_5 = contacts_data.elements[i].businessPhone;
+    
+    // result_item.ATTRIBUTE_6 = "";      //확인필요
+
+    // result_item.ATTRIBUTE_8 = "";      //넷중 하나 또는 4개의 필드 정보 합 ( 확인 필요 )
+    // result_item.ATTRIBUTE_10 = "";     //데이터 없음
+    // result_item.ATTRIBUTE_11 = "";     //사업부코드( 코드마스터 필요 ) 예) HE    LGE 앞자리 빼는지 확인 필요
+
+    // result_item.REGISTER_DATE = "";    //어떤 날짜 정보인지 확인 필요
+    if( contacts_data.elements[i].createdAt != undefined) result_item.REGISTER_DATE = utils.timeConverter("GET_DATE", contacts_data.elements[i].createdAt);
+
+    result_item.TRANSFER_DATE = moment().format('YYYY-MM-DD hh:mm:ss');    //어떤 날짜 정보인지 확인 필요
+
+    result_item.TRANSFER_FLAG = "Y";		//TRANSFER_FLAG N , Y 값의 용도 확인 필요
+
+    //result_item.LAST_UPDATE_DATE = ""; 
+    if( contacts_data.elements[i].updatedAt != undefined) result_item.LAST_UPDATE_DATE = utils.timeConverter("GET_DATE", contacts_data.elements[i].updatedAt);
+    
+    // result_item.API_G_CODE = "";       //API 구분코드 추가요건 사항
 
     result_data.push(result_item);
   }
