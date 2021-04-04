79dc5901bfd8a6c798a54dab8c6895dddb3776ee
jtlim101010 <jtlim101010@gmail.com>
2021-04-04 11:11:18 +0900

    스케줄러  시작

--- app.js
+++ app.js
@@ -196,7 +196,7 @@ app.use(function(err, req, res, next) {
 function schedule_Request(){
 	let uniqe_jobs_name = "B2B GERP GLOBAL" +  moment().format('YYYYMMDD');
 	let second = "0";
-	let minutes = "27";
+	let minutes = "30";
 	let hours = "11";
 	let dayofmonth = "*";
 	let month = "*";
@@ -215,10 +215,10 @@ function schedule_Request(){
 	});
 }
 
-// if(__dirname == "/home/opc/LGE/b2bgerp_global"){
-// 	console.log("reg gerp");
-// 	schedule_Request();
-// } 
+if(__dirname == "/home/opc/LGE/b2bgerp_global"){
+	console.log("reg gerp");
+	schedule_Request();
+} 
 
 if(os.type().indexOf("Windows") > -1) global.OS_TYPE = "Windows"
 else global.OS_TYPE = "Linux";
