5f65ba0f7241bfd061a6cc7fdd1604bc9f693856
jtlim101010 <jtlim101010@gmail.com>
2021-04-06 17:50:28 +0900

    스케줄러 중지

--- app.js
+++ app.js
@@ -214,10 +214,10 @@ function schedule_Request(){
 	});
 }
 
-if(__dirname == "/home/opc/LGE/b2bgerp_global"){
-	console.log("reg gerp");
-	schedule_Request();
-} 
+// if(__dirname == "/home/opc/LGE/b2bgerp_global"){
+// 	console.log("reg gerp");
+// 	schedule_Request();
+// } 
 
 if(os.type().indexOf("Windows") > -1) global.OS_TYPE = "Windows"
 else global.OS_TYPE = "Linux";
