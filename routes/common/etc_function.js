1702da4a734fd87129904e6b5d1ad5ca2e472aa0
gp-jtlim <jtlim@goldenplanet.co.kr>
2021-08-04 09:59:45 +0900

    B2B GERP GLOBAL MQL DATA안 넘어간 건으로 인하여 진행중

--- routes/common/etc_function.js
+++ routes/common/etc_function.js
@@ -15,6 +15,28 @@ router.get('/fields', function (req, res, next) {
 	});
 });
 
+router.get('/activities_log/:id', function (req, res, next) {
+	var queryString = {
+		type : "campaignMembership" ,
+		startDate : 1627398000,
+		endDate : 1627621200 ,
+		count : 2
+	}
+
+	///api/REST/1.0/data/activities/contact/{id}
+	console.log("call activity log");
+	let id = req.params.id;
+	console.log("activity logs : " +  req.params.id);
+
+	old_eloqua.data.activities.get(id , queryString).then((result) => {
+		console.log(result.data);
+		res.json(result.data);
+	}).catch((err) => {
+		console.error(err);
+	});
+});
+
+
 
 
 module.exports = router;
