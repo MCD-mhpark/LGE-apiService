795a94185817ab53d557281a9c8fa89b5eda6eaf
Lee-Chang-Min <10billionmin@naver.com>
2023-01-17 14:56:33 +0900

    수정 삽입 데이터 API 등록

--- routes/b2bgerp_kr_us/Data/contacts.js
+++ routes/b2bgerp_kr_us/Data/contacts.js
@@ -36,6 +36,22 @@ router.post('/langPageTest', function (req, res, next) {
     res.send('ㅇㅅㅇ');
 });
 
+router.get('/fields', function (req, res, next) {
+	var queryString = {
+		depth: "complete"
+		// search : "?isStandard=false"
+	}
+	//console.log(b2bgerp_eloqua.assets.contacts);
+    //Retrieve an external asset -> Method GET
+    //Create an external asset -> Method POST
+	lge_eloqua.assets.contacts.fields.get(queryString).then((result) => {
+		//console.log(result.data);
+		res.json(result.data);
+	}).catch((err) => {
+		console.error(err.message);
+	});
+});
+
 //=====================================================================================================================
 
 //const bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET);
@@ -211,8 +227,9 @@ router.get('/TestContactDataSearch', async function (req, res, next) {
 	let queryString = {}
 	let queryText = "";
 
-	let start_date = req.query.startDate
-	let end_date = req.query.endDate
+	let start_date = req.body.startDate
+	let end_date = req.body.endDate
+	let gubun = req.body.gubun
 
 	let createDt_Object = utils.yesterday_getDateTime();
 	start_date ? createDt_Object.start = start_date : null;
@@ -222,10 +239,10 @@ router.get('/TestContactDataSearch', async function (req, res, next) {
 	//C_DateCreated
 	//queryText = "emailAddress='jtlim@goldenplanet.co.kr'";
 	if(start_date != null && end_date != null){
-		queryText = "C_DateCreated>" + "'" + createDt_Object.start + " 00:00:00'" + "C_DateCreated<" + "'" + createDt_Object.end + " 23:59:59'";
+		queryText = gubun+">" + "'" + createDt_Object.start + " 00:00:00'" + gubun+"<" + "'" + createDt_Object.end + " 23:59:59'";
 		queryString['search'] = queryText;
 	}
-	queryString['depth'] = req.query.depth;
+	queryString['depth'] = req.body.depth;
 	
 	console.log("queryText : " + queryText);
 	await lge_eloqua.data.contacts.get(queryString).then((result) => {
@@ -261,24 +278,28 @@ router.get('/TestAccountDataSearch', async function (req, res, next) {
 /*{
 		startDate = '2023-01-01',
 		endDate = '2023-01-01',
-		depth = 'complete' ,'minimal', 'complete'
+		depth = 'complete' ,'minimal', 'partial'
 
 	}*/
 	let queryString = {}
 	let queryText = "";
 
-	let start_date = req.query.startDate
-	let end_date = req.query.endDate
+	let start_date = req.body.startDate
+	let end_date = req.body.endDate
+	let gubun = req.body.gubun
+
+	//M_DateCreated
+	//M_DateModified
 
 	let createDt_Object = utils.yesterday_getDateTime();
 	start_date ? createDt_Object.start = start_date : null;
 	end_date ? createDt_Object.end = end_date : null;
 
 	if(start_date != null && end_date != null){
-		queryText = "M_DateCreated>" + "'" + createDt_Object.start + " 00:00:00'" + "M_DateCreated<" + "'" + createDt_Object.end + " 23:59:59'";
+		queryText = gubun+">" + "'" + createDt_Object.start + " 00:00:00'" + gubun+"<" + "'" + createDt_Object.end + " 23:59:59'";
 		queryString['search'] = queryText;
 	}
-	queryString['depth'] = req.query.depth;
+	queryString['depth'] = req.body.depth;
 
 	//partial 
 	console.log("queryText : " + queryText);
